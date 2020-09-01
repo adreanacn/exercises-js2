7bb2ab9592ca21ca6c17b93162b0e6a03a742303
Adriana Ceron <adrianaceron@Adreana.home>
2020-09-01 18:45:05 +0200

    InClass A1

--- week-1/InClass/A-objects-intro/exercise-part-1.js
+++ week-1/InClass/A-objects-intro/exercise-part-1.js
@@ -17,3 +17,68 @@ var car = {
 
 */
 
+var cat = {
+    name: "Shiro",
+    color: "Blanco",
+    edad: 4,
+    peso: 4,
+    size: 50,
+    procedencia: "Barcelona"
+}
+
+console.log(cat)
+
+var camara1 = {
+    megapixeles: "16 MP",
+    apertura: 1.8,
+}
+
+var camara2 = {
+    megapixeles: "8 MP",
+    apertura: 1.9,
+}
+
+var camara3 = {
+    megapixeles: "32 MP",
+    apertura: 10.8,
+}
+
+
+
+var telefonoMovil = {
+    marca: "Iphone",
+    modelo: "10x",
+    year: 2020,
+    precio: 1000,
+    camaras: [
+        camara1,
+        camara2,
+        camara3
+    ]
+
+}
+console.log(telefonoMovil.camaras)
+
+
+var profesion1 = {
+    area: "ingeniero informatico",
+    duracion: 5,
+    sueldobase: 1200,
+}
+
+var profesion2 = {
+    area: "tecnico informatico",
+    duracion: 3,
+}
+
+var tipoDeContrato1 = "indefinido"
+var tipoDeContrato2 = "tiempo determinado"
+var tipoDeContrato3 = "reemplazo baja"
+var tipoDeContrato4 = "reemplazo vacaciones"
+
+var ofertaLaboral = {
+    area: "informatico",
+    experiencia: 2,
+    oferta: 1500,
+    tipoDeContrato: tipoDeContrato4
+}
